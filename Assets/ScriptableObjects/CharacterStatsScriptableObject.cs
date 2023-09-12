be25dc7d4ce4a7b614d6ff899dcba0d97eb2c590
Julian Gaitan <juliangaitan_h@hotmail.com>
2023-09-12 13:29:45 -0500

    Player Movement Mockup
    movement and jump of the player

---
+++ Assets/ScriptableObjects/CharacterStatsScriptableObject.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "CharacterStatsScriptableObject", menuName = "Character Stats")]
+public class CharacterStatsScriptableObject : ScriptableObject
+{
+    [Header("Movement system")]
+    public float maxSpeed;
+    public float acceleration;
+    public float deceleration;
+
+    [Header("Jump sistem")]
+    public float jumpForce;
+    public float fallMultiplier;
+    public float jumpTime;
+    public float jumpMultiplier;
+    public float jumpDecayPercentage;
+}
---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,54 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [SerializeField] private CharacterStatsScriptableObject characterStats;
+
+    private PlayerInputActions playerControls;
+    private InputAction move;
+
+    private Rigidbody2D rb;
+
+    private Vector2 playerInput;
+    private Vector2 desiredSpeed;
+    private Vector2 currentSpeed;
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        playerControls = new PlayerInputActions();
+    }
+
+    private void OnEnable()
+    {
+        move = playerControls.Player.Move;
+        move.Enable();
+    }
+
+    private void OnDisable()
+    {
+        move.Disable();
+    }
+
+    private void Update()
+    {
+        playerInput = move.ReadValue<Vector2>();
+    }
+
+    private void FixedUpdate()
+    {
+        if (playerInput != Vector2.zero)
+        {
+            desiredSpeed = new Vector2(playerInput.x * characterStats.maxSpeed, rb.velocity.y);
+            currentSpeed = rb.velocity;
+            rb.velocity = Vector2.Lerp(currentSpeed, desiredSpeed, characterStats.acceleration * Time.fixedDeltaTime);
+        }
+        else
+        {
+            desiredSpeed = new Vector2(0, rb.velocity.y);
+            currentSpeed = rb.velocity;
+            rb.velocity = Vector2.Lerp(currentSpeed, desiredSpeed, characterStats.deceleration * Time.fixedDeltaTime);
+        }
+    }
+}
+
