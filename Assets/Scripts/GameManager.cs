d69b65f3bfe5ddfa6cfbd4075589b46612f811f8
Julian Gaitan <juliangaitan_h@hotmail.com>
2023-09-12 15:47:06 -0500

    GameManager

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -4,15 +4,58 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public static GameManager Instance;
+
+    public Transform currentCheckpoint;
+    public GameObject playerPrefab;
+    private GameObject playerInstance;
+
+    private void Awake()
     {
-        
+        if (Instance == null)
+        {
+            Instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else if (Instance != this)
+        {
+            Destroy(gameObject);
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Start()
     {
-        
+        SpawnPlayer();
+    }
+    
+    public void UpdateCheckPoint(Transform checkpoint)
+    {
+        currentCheckpoint = checkpoint;
+    }
+
+    public void SpawnPlayer()
+    {
+        if (playerInstance != null)
+        {
+            Destroy(playerInstance);
+        }
+
+        playerInstance = Instantiate(playerPrefab, currentCheckpoint.position, currentCheckpoint.rotation);
+    }
+
+    [ContextMenu("Player Died")]
+    public void PlayerDied()
+    {
+        // logic for the player dying
+        SpawnPlayer();
+    }
+
+    private void Update()
+    {
+        if (Input.GetKey(KeyCode.K)) 
+        {
+            PlayerDied();
+        }
     }
 }
+
