dc3cde0025a92ab9ac505c743d02b65ac4bab2be
camilolc <kmilo3025@hotmail.com>
2023-09-13 10:55:19 -0500

    Flip movement and grab skill completed

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -13,6 +13,12 @@ public class PlayerMovement : MonoBehaviour
     private Vector2 playerInput;
     private Vector2 desiredSpeed;
     private Vector2 currentSpeed;
+    [Header("Flip info")]
+    public int facingDir = 1;
+    public bool isFacingRight = true;
+    public bool canFlip = true;
+
+    
     private void Awake()
     {
         rb = GetComponent<Rigidbody2D>();
@@ -42,6 +48,7 @@ public class PlayerMovement : MonoBehaviour
             desiredSpeed = new Vector2(playerInput.x * characterStats.maxSpeed, rb.velocity.y);
             currentSpeed = rb.velocity;
             rb.velocity = Vector2.Lerp(currentSpeed, desiredSpeed, characterStats.acceleration * Time.fixedDeltaTime);
+            FlipController(playerInput.x);
         }
         else
         {
@@ -50,5 +57,23 @@ public class PlayerMovement : MonoBehaviour
             rb.velocity = Vector2.Lerp(currentSpeed, desiredSpeed, characterStats.deceleration * Time.fixedDeltaTime);
         }
     }
+
+    #region FLip
+    private void Flip()
+    {
+        facingDir = facingDir * -1;
+        isFacingRight = !isFacingRight;
+        transform.Rotate(0, 180, 0);
+
+    }
+
+    private void FlipController(float _x)
+    {
+        if (_x > 0 && !isFacingRight && canFlip)
+            Flip();
+        else if (_x < 0 && isFacingRight && canFlip)
+            Flip();
+    }
+    #endregion
 }
 
