dacdf2182af37d5cf4cb736509a18d42e62e7ce9
Snowdencd <Snow-0202@hotmail.com>
2023-09-15 19:10:05 -0500

    Assets Climb Add
    Animations for Climbing and grab

--- Assets/Scripts/PlayerJump.cs
+++ Assets/Scripts/PlayerJump.cs
@@ -71,6 +71,7 @@ public class PlayerJump : MonoBehaviour
             isJumpPressed = false;
 
         animator.SetBool("OnGround", isGrounded());
+        animator.SetFloat("VerticalVelocity", rb.velocity.y);
     }
 
     void FixedUpdate()
